c21166b0f8a3e49ef930f2b69da9b2bbe01b8778
Tim1222 <timofeu47@gmail.com>
2025-06-12 17:30:26 +0200

    change useRef to useState and add rules for button

--- src/AddtaskForm.tsx
+++ src/AddtaskForm.tsx
@@ -1,4 +1,4 @@
-import {ChangeEvent, useRef, useState} from "react";
+import {useState} from "react";
 
 type AddTaskFormPropsType = {
     createTask: (title: string) => void
@@ -6,18 +6,34 @@ type AddTaskFormPropsType = {
 
 export const AddtaskForm = ({createTask}: AddTaskFormPropsType) => {
 
-    const inputRef = useRef<HTMLInputElement>(null)
-
+    //const inputRef = useRef<HTMLInputElement>(null)
+    const [taskInput, setTaskInput] = useState('')
 
     return <div>
-        <input ref={inputRef}/>
-        <button onClick={() => {
-            if (
-                inputRef.current) {
-                createTask(inputRef.current.value)
-                inputRef.current.value = ''
-            }
+
+        {/*<input ref={inputRef}/>*/}
+        {/*<button onClick={() => {*/}
+        {/*    if (*/}
+        {/*        inputRef.current) {*/}
+        {/*        createTask(inputRef.current.value)*/}
+        {/*        inputRef.current.value = ''*/}
+        {/*    }*/}
+        {/*}}>+*/}
+        {/*</button>*/}
+        <input
+            placeholder={'Max title 10 element'}
+            value={taskInput}
+            onChange={(e) => {
+            setTaskInput(e.currentTarget.value)
+            console.log(taskInput)}}/>
+        <button
+            disabled={!taskInput || taskInput.length > 10}
+            onClick={() => {
+            createTask(taskInput)
+            setTaskInput('')
         }}>+
         </button>
+        {taskInput && <div>Max title 10 element</div>}
+        {taskInput.length > 10 && <div style={{color: 'red'}}>Title is too long</div>}
     </div>
 }
